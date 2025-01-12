84f390df11ffc9a577c97d3b108afa16371f43ca
trybledteen <vikaoleynik202006@gmail.com>
2025-01-12 21:18:08 +0100

    Home.vue

---
+++ vue-pizza/src/views/Home.vue
@@ -0,0 +1,17 @@
+<script>
+import CardList from '@/components/CardList.vue';
+
+export default {
+  name: 'HomePage',
+  components: {
+    CardList
+  }
+}
+</script>
+
+<template>
+  <div class="p-10">
+    <h2 class="text-3xl font-bold mb-8">Pizza</h2>
+    <CardList />
+  </div>
+</template>
