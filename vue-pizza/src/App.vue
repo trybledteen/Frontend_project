397e5e2b900fc3469c7cc2179d46399166001b11
trybledteen <vikaoleynik202006@gmail.com>
2025-01-12 21:18:34 +0100

    App.vue

--- vue-pizza/src/App.vue
+++ vue-pizza/src/App.vue
@@ -1,47 +1,41 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import Header from './components/Header.vue'
+import Drawer from './components/Drawer.vue'
+
+export default {
+  name: 'AppLayout',
+  components: {
+    Header,
+    Drawer
+  },
+  data() {
+    return {
+      drawerOpen: false
+    }
+  },
+  methods: {
+    openDrawer() {
+      this.drawerOpen = true
+    },
+    closeDrawer() {
+      this.drawerOpen = false
+    }
+  },
+  provide() {
+    return {
+      cartActions: {
+        closeDrawer: this.closeDrawer,
+        openDrawer: this.openDrawer
+      }
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Drawer v-if="drawerOpen" @close="closeDrawer" />
+  <div class="bg-neutral-400 w-4/5 m-auto rounded-xl shadow-xl mt-14">
+    <Header @open-drawer="openDrawer" />
+    <router-view></router-view>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
